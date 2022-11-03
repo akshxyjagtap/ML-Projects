74e53b900172a3138461b9c78ff1b4c80a372cf6
Akshay Jagtap <78637034+akshxyjagtap@users.noreply.github.com>
2022-11-03 09:51:42 +0530

    formateed and readme created

---
+++ Hand Tracking/READMe.md
@@ -0,0 +1,7 @@
+**HANDS DETECTION**
+
+- Detecting hands using media pipe on cv2
+- reference :  - [MediaPipe Hands]( https://google.github.io/mediapipe/solutions/hands.html) .
+
+***Wanted to learn about hands detection using media pipe so undertook this project***
+ 
--- Hand Tracking/main.py
+++ Hand Tracking/main.py
@@ -7,26 +7,22 @@ mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
 
-
-
 cap = cv2.VideoCapture(0)
 with mp_hands.Hands(
-    model_complexity=0,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as hands:
+        model_complexity=0,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as hands:
   while cap.isOpened():
     success, image = cap.read()
     if not success:
       print("Ignoring empty camera frame.")
-      
+
       continue
 
-   
     image.flags.writeable = False
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     results = hands.process(image)
 
-    
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     if results.multi_hand_landmarks:
@@ -37,7 +33,7 @@ with mp_hands.Hands(
             mp_hands.HAND_CONNECTIONS,
             mp_drawing_styles.get_default_hand_landmarks_style(),
             mp_drawing_styles.get_default_hand_connections_style())
-  
+
     cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
     if cv2.waitKey(5) & 0xFF == 27:
       break
